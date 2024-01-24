22c7e3db051f33204842039af069e488720ef7bd
drozdovalena <drozdova.el.gen@gmail.com>
2024-01-24 15:31:36 +0100

    lesson logical operators hw1 range inclusive

---
+++ logical-operators/lo.js
@@ -0,0 +1,9 @@
+function getMessage(age) {
+    return age >= 14 && age <= 90;
+
+}
+
+document.write(getMessage(20), "<br>");
+document.write(getMessage(45), "<br>");
+document.write(getMessage(14), "<br>");
+document.write(getMessage(1000), "<br>");
