6639cbce287fb6c75b8941d0f26ae0f254f1b0db
drozdovalena <drozdova.el.gen@gmail.com>
2024-01-24 20:29:28 +0100

    lesson logical operators hw3

--- logical-operators/lo.js
+++ logical-operators/lo.js
@@ -1,11 +1,32 @@
-function getMessage(age) {
+function getMessage() {
     // return age >= 14 && age <= 90;  hw1
 
-   return age < 14 || age > 90;  //hw2
+   // return age < 14 || age > 90;  //hw2
 
+   // Напишите код, который будет спрашивать логин с помощью prompt.
+   //
+   //     Если посетитель вводит «Админ», то prompt запрашивает пароль, если ничего не введено или нажата клавиша Esc – показать «Отменено», в противном случае отобразить «Я вас не знаю».
+   //
+   // Пароль проверять так:
+   //
+   //     Если введён пароль «Я главный», то выводить «Здравствуйте!»,
+   // Иначе – «Неверный пароль»,
+   // При отмене или в случае если ничего не введено – «Отменено».
+
+   let login = prompt("Введите логин");
+   if(login==="Админ") {
+      let password = prompt("Введите пароль");
+      if(password===null || password==="") {
+         return "Отменено";
+      }else if (password==="Я главный"){
+         return "Здравствуйте!";
+      } else {
+         return "Неверный пароль";
+      }
+   }
 }
 
-document.write(getMessage(20), "<br>");
-document.write(getMessage(45), "<br>");
-document.write(getMessage(14), "<br>");
-document.write(getMessage(1000), "<br>");
+document.write(getMessage(), "<br>");
+document.write(getMessage(), "<br>");
+document.write(getMessage(), "<br>");
+document.write(getMessage(), "<br>");
